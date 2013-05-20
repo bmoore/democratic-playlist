5df1e28e2effcc17ef53035f3500bb93ca8a6921
Brian Moore <moore.brian.d@gmail.com>
2013-05-20 17:52:37 -0400

    converted artist to remove backbone

--- server/controllers/artist.js
+++ server/controllers/artist.js
@@ -1,36 +1,19 @@
 define([
-  'squel',
   'modules/artist',
-  'modules/song',
-  'modules/album',
-], function (Squel, Artist, Song, Album) {
+], function (Artist) {
 
   var ArtistController = {};
 
   ArtistController.artist = function(req, res) {
-    var artist = new Artist.Model();
-    var songs = new Song.Collection();
-    var albums = new Album.Collection();
-
-    artist.fetch({
-      where: "id = ?",
-      params: [req.params.id],
-      success: function(m, r, o) {
-        songs.fetch({
-          where: "artist_id = ?",
-          params: [artist.get('id')],
-          success: function(m, r, o) {
-            artist.set('songs', songs);
-            albums.fetch({
-              where: "id IN (SELECT album_id FROM song WHERE artist_id = ?)",
-              params: [artist.get('id')],
-              success: function(m, r, o) {
-                artist.set('albums', albums);
-                res.send(artist.toJSON());
-              }
-            });
-          }
-        });
+    Artist.get_artist({
+      values: [
+        req.params.id
+      ],
+      success: function(results) {
+        res.send(results);
+      },
+      error: function(err, query, options) {
+        res.send(500);
       }
     });
   };
--- server/modules/artist.js
+++ server/modules/artist.js
@@ -1,16 +1,33 @@
 define([
-  'core/modules/artist',
-  'lodash'
-], function(CoreArtist, _) {
-  var Artist = {};
-  _.extend(Artist, CoreArtist);
+  'server/tools/db',
+  'squel'
+], function(Db, Squel) {
+  var Artist = {
+    get_artist: function(options) {
+      var query = Squel.select().
+        from("artist").
+        left_join("song", null, "artist.id = song.artist_id").
+        left_join("album", null, "song.album_id = album.id").
+        where("artist.id = ?").
+        toString();
 
-  Artist.Model = CoreArtist.Model.extend({
-    initialize: function(args) {
-      CoreArtist.Model.prototype.initialize.call(this, args);
-      this.table = "artist";
+      var success = options.success;
+      options.success = function(results) {
+        var artist = results[0].artist;
+        artist.songs = [];
+        artist.albums = [];
+
+        for (i in results) {
+          artist.songs.push(results[i].song);
+          artist.albums.push(results[i].album);
+        }
+
+        if (success) success(artist);
+      };
+
+      Db.run({sql: query, nestTables: true}, options);
     }
-  });
+  };
 
   return Artist;
 });
